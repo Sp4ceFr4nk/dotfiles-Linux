a0943c0c6d1eb9d4730b6a8665bb066c662481fb
Sp4ceFr4nk <enrico.franco99@gmail.com>
2023-01-04 12:53:19 +0100

    Removed tray and added icons to polybar. Removed sleep time from launch

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,10 +6,6 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# wait until the process have shutdown
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-
 # Launch bar1 
 echo "---" | tee -a /tmp/polybar1.log 
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
